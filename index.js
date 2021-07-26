f6350a46e7d2bffcf6d886cfb1e9b203f7acbd2e
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-26 19:00:09 -0300

    Adiciona Pergunta das Moedas e Transforma em Ferramenta de CLI

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
 
 console.log('Bot Conversor de Moedas');
 
 async function bot(){
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  const baseCurrency = 'dolar';
-  const finalCurrency = 'real';
+  const baseCurrency = readlineSync.question('Informe uma moeda base: ') || 'dolar';
+  const finalCurrency = readlineSync.question('Informe uma moeda desejada: ') || 'real';
   const converterUrl = `https://www.google.com/search?q=${baseCurrency}+para+${finalCurrency}&rlz=1C1CHZL_pt-BRBR806BR806&oq=${baseCurrency}+para+${finalCurrency}&aqs=chrome..69i57j35i39j0i131i433l2j0i433j0i131i433j0j0i433l3.2799j0j7&sourceid=chrome&ie=UTF-8`;
   await page.goto(converterUrl);
 
